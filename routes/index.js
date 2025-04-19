ba4215bc9c8c0101d21a514339598d0b5be943b7
Siang Huang <sianglifen@gmail.com>
2025-04-19 11:35:20 +0800

    Update session reading

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", check_session, (req, res, next) => {
-  res.render("index", { title: `Welcome ${req.session.user}` });
+  res.render("index", {
+    title: `Welcome ${req.session.user.name}`,
+    admin: req.session.user.rule == 1,
+  });
 });
 
 module.exports = router;
